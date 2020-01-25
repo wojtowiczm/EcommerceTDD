51a216fa7a96b7672741af6ea58097fd2646ef9f
Michal Wojtowicz <wojtowiczmichal97@gmail.com>
2020-01-25 12:36:05 +0100

    init

---
+++ src/main/java/pl/wojtowicz/ebook/Application.java
@@ -0,0 +1,12 @@
+package pl.wojtowicz.ebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
