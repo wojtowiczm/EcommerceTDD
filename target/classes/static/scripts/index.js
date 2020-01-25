50457816722e26b2137d3e749725ad207300e6f1
Michal Wojtowicz <wojtowiczmichal97@gmail.com>
2020-01-25 15:56:06 +0100

    Added some js

---
+++ target/classes/static/scripts/index.js
@@ -0,0 +1,35 @@
+
+function getProducts() {
+    return fetch("/api/products").then((response) => response.json())
+}
+
+function toProductHtmlElement(product) {
+    let htmlElement = document.createElement('<li>')
+
+    let innerHtml = `
+            <h2>${product.title}</h2>
+            <span>${product.price}</span>
+            <button data-product-id=${product.id}>Add me</button>
+    `;
+    htmlElement.innerHtml = innerHtml.trim()
+    return htmlElement
+}
+
+function bindButton(element) {
+    element.querySelector('button').addEventListener('click', (event) => {
+        const productId = e.target.getAttribute()
+        console.log()
+    })
+    return element
+}
+function main() {
+    const productsList = document.querySelector('product-list')
+
+    getProducts().then((products) => {
+        products
+        .map(toProductHtmlElement)
+        .map(bindButton)
+        .forEach(productsList.appendChild)
+    })
+}
+main();
